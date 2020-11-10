4f6c61aec1625e312587f7fe109d876e3e3bd04f
eirikano <31767381+eirikano@users.noreply.github.com>
2020-11-10 20:05:01 +0100

    many fixes

--- script/script.js
+++ script/script.js
@@ -3,20 +3,37 @@
 var link_box = document.getElementsByClassName("linkBox")
 var portrait_img = document.getElementById("portraitImg")
 var portrait_box = document.getElementById("portraitBox")
+var blue_box = document.getElementById("blueBox")
+var blue_box_inner = document.getElementById("blueBoxInner")
+var about_me = document.getElementById("aboutMe")
+
 
 function portrait(event) {
     switch (event) {
         case "click":
             portrait_box.classList.add("expandCircle")
             portrait_img.classList.add("portraitFade")
+            blue_box.style.animation = "formFadeOut"
+            blue_box.style.animationDuration = "1s"
+            blue_box.style.transition = "1s"
+            setTimeout(function () {
+                blue_box.style.display = "none"
+                blue_box_inner.style.display = "none"
+                about_me.style.display = "flex"
+                portrait_box.style.display = "none"
+            }, 950);
+
+            blue_box_inner.style.animation = "formFadeOut"
+            blue_box_inner.style.animationDuration = "1s"
+            blue_box_inner.style.transition = "1s"
             break
 
         case "enter":
-            portrait_img.setAttribute('src', "img/portrait_color.jpg")
+            portrait_img.style.filter = "grayscale(0%)"
             break
 
         case "leave":
-            portrait_img.setAttribute('src', "img/portrait.jpg")
+            portrait_img.style.filter = "grayscale(100%)"
             break
     }
 
@@ -28,15 +45,19 @@ portrait_img.addEventListener("mouseenter", function () { portrait("enter"); })
 portrait_img.addEventListener("mouseleave", function () { portrait("leave"); })
 portrait_img.addEventListener("click", function () { portrait("click"); })
 
-var blue_box = document.getElementById("blueBox")
-var blue_box_inner = document.getElementById("blueBoxInner")
+
 function onclick_form(form_type) {
     // var body_element = document.getElementsByTagName("body")[0]
+
     if (form_type == "CV") {
         blue_box.style.display = "flex"
         blue_box.style.animation = "formFadeIn"
         blue_box.style.animationDuration = "1s"
         blue_box.style.transition = "1s"
+        about_me.style.display = "none"
+        portrait_box.classList.remove("expandCircle")
+        portrait_img.classList.remove("portraitFade")
+        portrait_box.style.display = "flex"
     }
     else if (form_type == "Front") {
         blue_box.style.animation = "formFadeOut"
@@ -46,7 +67,8 @@ function onclick_form(form_type) {
         blue_box_inner.style.animation = "formFadeOut"
         blue_box_inner.style.animationDuration = "1s"
         blue_box_inner.style.transition = "1s"
-
+        about_me.style.display = "none"
+        portrait_box.style.display = "flex"
         portrait_box.classList.remove("expandCircle")
         portrait_img.classList.remove("portraitFade")
         setTimeout(function () {
